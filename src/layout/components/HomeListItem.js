1fd076bb7cae6aa1bd2abdbbae2a7995e185a8d6
Carlos Barata <baratac@gmail.com>
2020-03-19 19:23:32 +0000

    Dockers experience
    Created container for client app
    update package list
    change main table highlight colors

--- src/layout/components/HomeListItem.js
+++ src/layout/components/HomeListItem.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function HomeListItem(props) {
     return (
-        <div className="flex flex-col sm:flex-row w-full text-gray-800 hover:bg-green-600 hover:text-gray-100 hover:font-semibold">
+        <div className="flex flex-col sm:flex-row w-full text-gray-800 hover:bg-orange-200 hover:text-gray-900 hover:font-semibold">
             <div className="flex flex-col sm:flex-row content-center w-full sm:w-1/5">
                 <p className="sm:hidden block font-semibold pt-2 pb-1 bg-green-200">Title</p>
                 <p className="p-2 self-center font-semibold">{props.item.name}</p>
@@ -26,7 +26,7 @@ export default function HomeListItem(props) {
                     (platform, index) => (
                         <Link 
                             to={'/campaign?id=' + props.item.id + '&platform=' + platform} 
-                            className="pb-1 self-center text-blue-700 underline hover:text-blue-200" key={index}>{platform}
+                            className="pb-1 self-center text-blue-700 underline hover:text-blue-900" key={index}>{platform}
                         </Link>
                     )
                 )}
