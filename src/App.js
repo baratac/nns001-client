f1efa3363794adb3c8b068c95d79db43be4a870c
Carlos Barata <baratac@gmail.com>
2020-03-22 21:54:46 +0000

    Introducing Spinners
    Spinner while waiting for data on startup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
+import { ImpulseSpinner } from 'react-spinners-kit';
 
 import Home from './layout/HomePage';
 import Platform from './layout/PlatformPage';
@@ -8,11 +9,13 @@ import NotFound from './layout/NotFoundPage'
 
 function App() {
   const [cpgList, setCpgList] = useState([])
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (cpgList.length === 0 ) {
       axios.get('/')
       .then( res => {
+        setLoading(false);
         setCpgList(res.data);
       })
       .catch( error => {
@@ -24,7 +27,10 @@ function App() {
 
   return (
     <Router>
-      <div className="container h-screen w-full mx-auto bg-gray-200">
+      <div className="container relative h-screen w-full mx-auto bg-gray-200">
+        <div className="absolute z-50" style={{top:"140px", left:"120px"}}>
+          <ImpulseSpinner size={30} color="#686769" loading={loading} />
+        </div>
         <Switch>
           <Route exact path="/">
             <Home theList={ cpgList }/>
